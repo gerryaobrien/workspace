f57363a8e6d1c0057f24cae245e7c570f8932829
gerryaobrien <gerry.obrien@microsoft.com>
2015-05-06 06:29:41 -0700

    Additions to Program
    Worked through chapter 3, 4, and 5

--- GeoQuiz2/src/com/acumenit/geoquiz2/QuizActivity.java
+++ GeoQuiz2/src/com/acumenit/geoquiz2/QuizActivity.java
@@ -2,6 +2,7 @@ package com.acumenit.geoquiz2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,6 +18,9 @@ public class QuizActivity extends Activity {
 	private Button mNextButton;
 	private Button mPrevButton;
 	private TextView mQuestionTextView;
+	private static final String TAG = "QuizActivity";
+	private static final String KEY_INDEX = "index";
+	
 	
 	private TrueFalse[] mQuestionBank = new TrueFalse[] {
 			new TrueFalse(R.string.question_oceans, true),
@@ -31,13 +35,18 @@ public class QuizActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+        
         setContentView(R.layout.activity_quiz);
         
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        if(savedInstanceState != null) {
+        	mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         updateQuestion();
         
         
-        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton = (Button)findViewById(R.id.true_button);        
         mTrueButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -120,5 +129,42 @@ public class QuizActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    public void onStart() {
+    	super.onStart();
+    	Log.d(TAG, "onStart() called");
+    }
+    
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	Log.d(TAG, "onPause() called");
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	Log.d(TAG, "onResume() called");
+    }
+    
+    @Override
+    public void onStop() {
+    	super.onStop();
+    	Log.d(TAG, "onStop() called");
+    }
+    
+    @Override
+    public void onDestroy() {
+    	super.onDestroy();
+    	Log.d(TAG, "onDestroy() called");
+    }
+    
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+    	super.onSaveInstanceState(savedInstanceState);
+    	Log.i(TAG, "onSaveInstanceState");
+    	savedInstanceState.putInt(KEY_INDEX,  mCurrentIndex);
     }
 }
